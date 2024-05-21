e0d50fe3d94f9a78bdc72af741705c833cfd4113
Zixck <162469835+Zixck@users.noreply.github.com>
2024-05-20 19:08:17 -0500

    Add files via upload

---
+++ WebApplication1/Controllers/DropdownController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DropdownController : Controller
+    {
+        public ActionResult Electrodomesticos()
+        {
+            return View();
+        }
+
+        public ActionResult RopayAccesorios()
+        {
+            return View();
+        }
+
+        public ActionResult HogaryMuebles()
+        {
+            return View();
+        }
+
+        public ActionResult Tecnologia()
+        {
+            return View();
+        }
+
+        public ActionResult Deporte()
+        {
+            return View();
+        }
+    }
+
+}
